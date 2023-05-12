bba9d4cd1e64aee3920ac9c36b743c61d85404a9
jusar23 <120343933+jusar23@users.noreply.github.com>
2023-05-12 11:46:15 -0500

    rick and morty

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import style from './Adout.module.css'
+
+const About = () => {
+  return (
+    <div className={style.contenedor}>
+     
+      <h1>pagina creada por:</h1>
+      <h1> juan sebastian arias quintero</h1>
+    </div>
+  );
+};
+
+export default About;
